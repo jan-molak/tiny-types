cc5b8b4604d9ad8966c7caca5fd87d4feddc6d0e
jan-molak <jan.molak@gmail.com>
2018-02-12 11:33:09 +0000

    test(pattern-matching): Test to ensure that the MatchesAnything rule matches any input
    MatchesAnything is used as a default rule (else) by all the PatternMatchers, so it has to always
    match any input should other rules could not be matched.

---
+++ spec/pattern-matching/rules/MatchesAnything.spec.ts
@@ -0,0 +1,16 @@
+import 'mocha';
+import { given } from 'mocha-testdata';
+import { MatchesAnything } from '../../../src/pattern-matching/rules';
+import { expect } from '../../expect';
+
+describe('MatchesAnything', () => {
+
+    given(
+        1, false, {},
+    ).
+    it('is always executed', (input: any) => {
+        const rule = new MatchesAnything(_ => _);
+
+        expect(rule.matches(input)).to.be.true;                              // tslint:disable-line:no-unused-expression
+    });
+});
